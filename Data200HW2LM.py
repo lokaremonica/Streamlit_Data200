d7e36efc2f560a390825916f2c1507437886b170
lokaremonica <43637284+lokaremonica@users.noreply.github.com>
2023-09-14 20:21:21 -0700

    Added python file
    This is a .py file to display two columns in the streamlit

---
+++ Data200HW2LM.py
@@ -0,0 +1,12 @@
+"""
+Program to display first and second column
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
