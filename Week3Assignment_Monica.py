e5bcaf9620d8bd34fc6de853f0abd565c5068b6e
lokaremonica <43637284+lokaremonica@users.noreply.github.com>
2023-09-14 20:32:47 -0700

    added .py file to display the graph

---
+++ Week3Assignment_Monica.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('mountain_flowers.csv')
+df = pd.DataFrame(data)
+#creating two seperate bar graphs for length and width of flower petals
+X = list(df.iloc[:, 2])
+Y = list(df.iloc[:,0])
+Y1 = list(df.iloc[:,1])
+plt.title("Bar graph Mountain Flowers vs Petal Length")
+plt.xlabel("Flowers")
+plt.bar(X, Y, color='g')
+plt.ylabel("Petal Length")
+plt.show()
+plt.title("Bar graph Mountain Flowers vs Petal Width")
+plt.xlabel("Flowers")
+plt.bar(X,Y1, color='r')
+plt.ylabel("Petal Width")
+plt.show()
