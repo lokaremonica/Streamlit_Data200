e56f376f865ca7d3b0e8947e158c35794100ecba
lokaremonica <43637284+lokaremonica@users.noreply.github.com>
2023-09-19 14:39:41 -0700

    Add files via upload

--- Week3Assignment_Monica.py
+++ Week3Assignment_Monica.py
@@ -1,7 +1,9 @@
-import matplotlib.pyplot as plt
-import pandas as pd
+"""
+Program to display bar graph for mountain flowers
+"""
 import streamlit as st
-
+import pandas as pd
+import matplotlib.pyplot as plt
 data = pd.read_csv('mountain_flowers.csv')
 df = pd.DataFrame(data)
 #creating two seperate bar graphs for length and width of flower petals
