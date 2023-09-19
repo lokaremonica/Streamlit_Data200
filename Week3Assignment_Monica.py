7acd6ba904b19100093950ed849dac8db1f81da8
lokaremonica <43637284+lokaremonica@users.noreply.github.com>
2023-09-19 15:42:10 -0700

    Update Week3Assignment_Monica.py

--- Week3Assignment_Monica.py
+++ Week3Assignment_Monica.py
@@ -1,27 +1,17 @@
 """
 Program to display bar graph for mountain flowers
 """
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
 data = pd.read_csv('mountain_flowers.csv')
 df = pd.DataFrame(data)
-
 #creating two seperate bar graphs for length and width of flower petals
 X = list(df.iloc[:, 2])
 Y = list(df.iloc[:,0])
 Y1 = list(df.iloc[:,1])
-
 plt.title("Bar graph Mountain Flowers vs Petal Length")
 plt.xlabel("Flowers")
 plt.bar(X, Y, color='g')
 plt.ylabel("Petal Length")
-plt.show()
-
-plt.title("Bar graph Mountain Flowers vs Petal Width")
-plt.xlabel("Flowers")
-plt.bar(X,Y1, color='r')
-plt.ylabel("Petal Width")
 plt.show()
